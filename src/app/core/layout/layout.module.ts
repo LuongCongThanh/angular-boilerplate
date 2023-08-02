a6dcd3f2b6000081e116b0b9d758aaa32c65f2c0
ThanhLuongCong <thanhluong.it.0909@gmail.com>
2023-08-02 23:41:32 +0700

    fix layout service

--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 
 import { MainLayoutComponent } from '@layout/main-layout/main-layout.component';
 import { PrimaryLayoutComponent } from './primary-layout/primary-layout.component';
-import { LayoutService } from '@core/service/layout.service';
+import { LayoutService } from '@core/service/layout/layout.service';
 
 @NgModule({
   declarations: [MainLayoutComponent, PrimaryLayoutComponent],
