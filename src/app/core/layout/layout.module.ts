f8b70ebedf2781ff9e6a7fa10ce355fd8eed953c
ThanhLuongCong <thanhluong.it.0909@gmail.com>
2023-09-21 00:32:18 +0700

    config layout

--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
-import { MainLayoutComponent } from '@layout/main-layout/main-layout.component';
-import { PrimaryLayoutComponent } from './primary-layout/primary-layout.component';
 import { LayoutService } from '@core/service/layout.service';
 
+import { MainLayoutComponent } from '@layout/main-layout/main-layout.component';
+import { PrimaryLayoutComponent } from './primary-layout/primary-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @NgModule({
-	declarations: [MainLayoutComponent, PrimaryLayoutComponent],
 	imports: [CommonModule, RouterModule],
 	exports: [],
 	providers: [LayoutService],
+	declarations: [MainLayoutComponent, PrimaryLayoutComponent, HeaderComponent, FooterComponent],
 })
 export class LayoutModule {}
