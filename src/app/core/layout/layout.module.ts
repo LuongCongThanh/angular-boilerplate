44d89aaf5597979879052f1e3dd23e3a1283ac4b
ThanhLuongCong <thanhluong.it.0909@gmail.com>
2023-10-02 23:07:05 +0700

    config i18n

--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { LayoutService } from '@core/service/layout.service';
 
 import { MainLayoutComponent } from '@layout/main-layout/main-layout.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { InputSwitchModule } from 'primeng/inputswitch';
 import { PrimaryLayoutComponent } from './primary-layout/primary-layout.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-	imports: [CommonModule, RouterModule],
+	imports: [CommonModule, RouterModule, TranslateModule, InputSwitchModule, FormsModule],
 	exports: [],
 	providers: [LayoutService],
 	declarations: [MainLayoutComponent, PrimaryLayoutComponent, HeaderComponent, FooterComponent],
